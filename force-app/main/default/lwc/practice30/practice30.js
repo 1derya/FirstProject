ba0d7f777bc3115c624569b539ed784f638c2404
Derya <safiyeb70@gmail.com>
2023-06-06 14:59:03 -0500

    LCW compose CommunicateFromParentToChild  practice

--- force-app/main/default/lwc/practice30/practice30.js
+++ force-app/main/default/lwc/practice30/practice30.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+export default class Practice30 extends LightningElement {
 
-export default class Practice30 extends LightningElement {}
+    inputMsg; 
+
+    handleChange(event) {
+        this.inputMsg = event.target.value; 
+    }
+
+}
