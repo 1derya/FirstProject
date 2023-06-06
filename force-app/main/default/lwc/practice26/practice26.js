d8a3117efbab9a8e6c1a7e4f5fdfa0b6ef6cb7d8
Derya <safiyeb70@gmail.com>
2023-06-06 12:18:06 -0500

    LWC deleteoutomatically parent child

--- force-app/main/default/lwc/practice26/practice26.js
+++ force-app/main/default/lwc/practice26/practice26.js
@@ -3,8 +3,24 @@ import { LightningElement, wire } from "lwc";
 import NAME_FIELD from "@salesforce/schema/Opportunity.Name";
 import STAGE_FIELD from "@salesforce/schema/Opportunity.StageName";
 import CLOSE_DATE_FIELD from "@salesforce/schema/Opportunity.CloseDate";
+import USER_ID  from '@salesforce/user/Id';
+
+import USERNAME_FIELD from "@salesforce/schema/User.Username";
+import EMAIL_FIELD from "@salesforce/schema/User.Email";
 
 export default class Practice26 extends LightningElement {
+
+    userId = USER_ID;//
+
+    @wire(getRecord, {recordId: '$userId', fields:[USERNAME_FIELD,EMAIL_FIELD] })
+    currentUser;
+
+    get username(){
+        return getFieldValue(this.currentUser.data, USERNAME_FIELD)
+    }
+    get userEmail() {
+        return getFieldValue(this.currentUser.data, EMAIL_FIELD)
+    }
 
 
     recordId = '006Dn00000946PnIAI';  // use your own recordId
@@ -16,7 +32,10 @@ export default class Practice26 extends LightningElement {
     // one common js method is getRecord( recordId , fields you want in array)
     // and we need to use @wire to wire that into property or function
 
-    @wire(getRecord, {recordId : '$recordId' , fields:[NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD]})
+    @wire(getRecord, {
+        recordId : '$recordId' , 
+        fields:[NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD]
+    })
     opp;
 
     //easier way to get the field value out of the wire result from getRecord method is 
@@ -31,8 +50,8 @@ export default class Practice26 extends LightningElement {
     }
 
     // Create a getter to get string version of opp property above 
-    get oppInString() {
-        return JSON.stringify(this.opp, null, 2); 
-    }
+    // get oppInString() {
+    //     return JSON.stringify(this.opp, null, 2); 
+    // }
 
 }
