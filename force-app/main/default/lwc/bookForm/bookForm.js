d005f8947fc32d60b0d8d43fc530a517b84aa984
Derya <safiyeb70@gmail.com>
2023-06-08 14:24:52 -0500

    LWC form parent child detail form

---
+++ force-app/main/default/lwc/bookForm/bookForm.js
@@ -0,0 +1,10 @@
+import { LightningElement } from "lwc";
+import BOOK_OBJECT from '@salesforce/schema/Book__c';
+
+
+export default class BookForm extends LightningElement {
+
+   recordId = 'a01Dn00000K7oQ6IAJ'
+   objectApiName= BOOK_OBJECT;
+
+}
