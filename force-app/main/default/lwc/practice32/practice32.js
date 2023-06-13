7025c0c55ab7afc954b976ad4f2242f1e94508a4
Derya <safiyeb70@gmail.com>
2023-06-13 09:55:25 -0500

    Lwc change ARRAY with {...   }

--- force-app/main/default/lwc/practice32/practice32.js
+++ force-app/main/default/lwc/practice32/practice32.js
@@ -7,7 +7,7 @@ export default class Practice32 extends LightningElement {
     //the properties of an object 
     //or to the elements of an array, 
     //decorate the field with @track.
-    @track
+    //@track
     person = {
         firstName: 'Jon', 
         lastName : 'Snow'
@@ -19,20 +19,24 @@ export default class Practice32 extends LightningElement {
     
     handleClick() {
         //this.lastName = 'Wick'; 
-        this.person.lastName = 'Wick'; 
+        //this.person.lastName = 'Wick'; 
         // if you do below you do not need @track
         // because reassigning the property value will re-render the page 
         // this.person = {
         //     firstName: 'Jon', 
         //     lastName : 'Wick'
         // };
+        this.person = {...this.person , lastName : 'Wick' };
     }
 
     changeFirstItem() {
-         this.sports[0] = 'CODING !!!';
+        // this.sports[0] = 'CODING !!!';
         // if you do below you do not need @track
         // because reassigning the property value will re-render the page 
         // this.sports =  ['CODING !!!','football','basketball'];
+        const sportsCopy = [...this.sports];
+        sportsCopy[0] = 'CODING !!!';
+        this.sports = sportsCopy;
     }
 
 }
